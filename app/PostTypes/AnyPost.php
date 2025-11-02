0aee8a00bd39baa48ac0d4882715ad7d47c93596
Martin <martinplr@gmail.com>
2025-11-02 22:51:32 +0100

    feat(AnyPost) convert in to post type class if exist, add whereType scope

--- app/PostTypes/AnyPost.php
+++ app/PostTypes/AnyPost.php
@@ -2,6 +2,33 @@
 
 namespace App\PostTypes;
 
-// TODO: Process into wanted classes if they exist
-// TODO: Add dedicated where post type scope
-class AnyPost extends PostType {}
+use App\PostTypeRegistry;
+
+class AnyPost extends PostType
+{
+    public function newFromBuilder($attributes = [], $connection = null)
+    {
+        $class = app(PostTypeRegistry::class)->find($attributes->type)['class'];
+        if ($class) {
+            $model = new $class;
+            $model = $model->newInstance([], true);
+        } else {
+            $model = $this->newInstance([], true);
+        }
+
+        $model->setRawAttributes((array) $attributes, true);
+
+        $model->setConnection($connection ?? $this->getConnectionName());
+
+        $model->fireModelEvent('retrieved', false);
+
+        return $model;
+    }
+
+    public function scopeWhereType($query, $type)
+    {
+        return is_array($type)
+            ? $query->whereIn('type', $type)
+            : $query->where('type', $type);
+    }
+}
