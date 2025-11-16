a00eb582dfd9a4cabc8493d673ee59c26630c213
Martin <martinplr@gmail.com>
2025-11-16 12:47:21 +0100

    chore: remove volt

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\VoltServiceProvider::class,
     App\Providers\PluginServiceProvider::class,
     App\Providers\ThemeServiceProvider::class,
     App\Providers\PostTypeServiceProvider::class,
