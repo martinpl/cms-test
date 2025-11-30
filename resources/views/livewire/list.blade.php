7b1dcffc537da8bc36026f80eb53c2d272df59dd
Martin <martinplr@gmail.com>
2025-11-30 17:26:38 +0100

    feat(list) add base filtering, actions

--- resources/views/livewire/list.blade.php
+++ resources/views/livewire/list.blade.php
@@ -1,6 +1,7 @@
 <?php
  
 use App\PostTypes\AnyPost;
+use Livewire\Attributes\Url;
 use Livewire\WithPagination;
 use Livewire\Attributes\Computed;
  
@@ -9,29 +10,124 @@ new class extends Livewire\Component {
 
     public $postType; 
 
+    #[Url(except: ['publish', 'draft'])]
+    public $status = ['publish', 'draft']; 
+
+    #[Url]
+    public $author; 
+
+    #[Url]
+    public $search;
+
     #[Computed]
     public function posts() {
         return AnyPost::where('type', $this->postType['name'])
+            ->whereStatus($this->status)
+            ->when($this->author, function ($query) {
+                $query->whereUserId($this->author);
+            })
+            ->when($this->search, function ($query) {
+                $query->search($this->search);
+            })
             ->latest()
             ->paginate(10);
+    }
+
+    public function setStatus($status) {
+        $this->author = null;
+        $this->status = $status;
+    }
+
+    public function trash($postId) {
+        AnyPost::find($postId)->trash();
+    }
+
+    public function restore($postId) {
+        AnyPost::find($postId)->untrash();
+    }
+
+    public function destroy($postId) 
+    {
+        AnyPost::destroy($postId);
     }
 } ?>
 
 <div>
-    <h2>
-        {{ $postType['plural'] }}
-    </h2>
-    <a href="{{ route('editor', $postType['name']) }}">
-        Add
-    </a>
+    <div class="flex gap-2">
+        <h2>
+            {{ $postType['plural'] }}
+        </h2>
+        <a href="{{ route('editor', $postType['name']) }}">
+            Add
+        </a>
+    </div>
+    @php
+        $allCount = AnyPost::whereType($this->postType['name'])->whereStatus(['publish', 'draft'])->count();
+        $mineCount = AnyPost::whereType($this->postType['name'])->whereStatus(['publish', 'draft'])->whereUserId(auth()->id())->count();;
+    @endphp
+    <div>
+        <a href="todo" wire:click.prevent="setStatus(['publish', 'draft']);">
+            All ({{ $allCount }})
+        </a> 
+        @if ($allCount != $mineCount)
+            <a href="todo" wire:click.prevent="$set('author', {{ auth()->id() }})">
+                Mine ({{ $mineCount }})
+            </a>
+        @endif
+        <a href="todo" wire:click.prevent="setStatus('publish')">
+            Published  ({{ AnyPost::whereType($this->postType['name'])->whereStatus('publish')->count() }})
+        </a>
+        <a href="todo" wire:click.prevent="setStatus('draft')">
+            Draft ({{ AnyPost::whereType($this->postType['name'])->whereStatus('draft')->count() }})
+        </a> | 
+        <a href="todo" wire:click.prevent="setStatus('trash')">
+            Trash ({{ AnyPost::whereType($this->postType['name'])->whereStatus('trash')->count() }})
+        </a>
+        <input type="search" wire:model.change="search">
+    </div>
     @foreach ($this->posts as $post)
         <div>
-            <a href="{{ route('editor', [$postType['name'], $post->id]) }}">
-                {{ $post->title }}
-            </a>
-            @if ($post->link())
-                <a href="{{ $post->link() }}">Preview</a>
-            @endif
+            {{-- TODO: Add select --}}
+            <div>
+                <a href="{{ route('editor', [$postType['name'], $post->id]) }}">
+                    {{ $post->title }}
+                </a>
+                @if ($post->status == 'draft')
+                    Draft
+                @endif
+            </div>
+            <div class="flex gap-2">
+                @if ($status != 'trash')
+                    <a href="{{ route('editor', [$postType['name'], $post->id]) }}">
+                        Edit
+                    </a>
+                    <button wire:click="trash({{ $post->id }})">
+                        Trash
+                    </button>
+                    {{-- TODO --}}
+                    {{-- <button>
+                        Duplicate
+                    </button> --}}
+                    @if ($post->link())
+                    {{-- TODO --}}
+                        <a href="{{ $post->link() }}">
+                            @if ($post->status == 'draft')
+                                Preview
+                            @else 
+                                View
+                            @endif
+                        </a>
+                    @endif
+                @endif
+                @if ($status == 'trash')
+                    <button wire:click="restore({{ $post->id }})">
+                        Restore
+                    </button>
+                    <button wire:click="destroy({{ $post->id }})">
+                        Delete Permanently
+                    </button>
+                @endif
+            </div>
         </div>
         <hr>
     @endforeach
