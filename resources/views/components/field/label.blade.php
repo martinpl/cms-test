31ad72e010db991d0ed6a43ce9cb753de86c22b2
Martin <martinplr@gmail.com>
2025-12-09 22:01:09 +0100

    feat(field.label) create component

---
+++ resources/views/components/field/label.blade.php
@@ -0,0 +1,10 @@
+@props([
+    'tag' => 'label',
+    'base' => "group/field-label peer/field-label flex w-fit gap-2 leading-snug group-data-[disabled=true]/field:opacity-50
+        has-[>[data-slot=field]]:w-full has-[>[data-slot=field]]:flex-col has-[>[data-slot=field]]:rounded-md has-[>[data-slot=field]]:border [&>*]:data-[slot=field]:p-4
+        has-data-[state=checked]:bg-primary/5 has-data-[state=checked]:border-primary dark:has-data-[state=checked]:bg-primary/10",
+])
+
+<x-label :$tag {{ $attributes->class($base) }}>
+    {{ $slot }}
+</x-label>
