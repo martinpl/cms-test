46cce210edcfb3ee525514932789dedf57a051e0
Martin <martinplr@gmail.com>
2025-12-09 21:57:18 +0100

    chore(button) use x-tag and $attributes->buttonTag()

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,12 +1,10 @@
 @props([
+    'base' => "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
     'variant' => 'default',
     'size' => 'default'
 ])
 
-<?php
-
-$tag = $attributes->has('a') ? 'button' : 'a';
-$base = "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive";
+@php
 $styles = [
     'variant' => [
         'default' => "bg-primary text-primary-foreground hover:bg-primary/90",
@@ -25,9 +23,8 @@ $styles = [
         "icon-lg" => "size-10",
     ],
 ];
+@endphp
 
-?>
-
-<{{ $tag }} {{ $attributes->class("{$base} {$styles['variant'][$variant]} {$styles['size'][$size]}") }}>
+<x-tag :tag="$attributes->buttonTag()" {{ $attributes->class([$base, $styles['variant'][$variant], $styles['size'][$size]]) }}>
     {{ $slot }}
-</{{ $tag }}>
+</x-tag>
